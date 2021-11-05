8460c7362bb32b818d3080ba109f8f8ff1ee500b
Tony3929 <t108360109@ntut.org.tw>
2021-11-05 20:05:59 +0800

    first

---
+++ app/src/main/java/com/example/lab5/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+
+}
